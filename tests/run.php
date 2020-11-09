e77f9e6b3ea0b0b2b06ff9ed50d66cafff68c34e
Kyrie Kopczynski <kyrie.ak@gmail.com>
2020-11-08 16:03:12 -0800

    Add formatted rows and colors

--- tests/run.php
+++ tests/run.php
@@ -15,14 +15,54 @@ $yamlRaw = file_get_contents($yamlFilePath);
 $parsedYaml = Yaml::parseFile(__DIR__ . '/../src/test.yaml');
 
 $rawBlocks = preg_split('/[\r\n]+(?=\S)/', $yamlRaw);
+$BLUE = "\033[0;34m";
+$GREEN = "\033[0;32m";
+$RESET = "\033[0;0m";
+
+function blockToFormattedRows($yaml, $parsedYaml) {
+  $columnSize = 40;
+  $yamlRows = explode("\n", $yaml);
+  $jsonRows = explode("\n", json_encode($parsedYaml, JSON_PRETTY_PRINT));
+  $maxYamlIndex = count($yamlRows) - 1;
+  $maxJSONIndex = count($jsonRows) - 1;
+  $maxIndex = max($maxYamlIndex, $maxJSONIndex);
+  $rows = [];
+
+  for ($i = 0; $i <= $maxIndex; $i++) {
+    $row = [];
+
+    if ($i > $maxYamlIndex) {
+      $row['yaml'] = str_pad('', $columnSize);
+    } else {
+      $row['yaml'] = str_pad($yamlRows[$i], $columnSize);
+    }
+
+    if ($i > $maxJSONIndex) {
+      $row['parsed'] = str_pad('', $columnSize);
+    } else {
+      $row['parsed'] = str_pad($jsonRows[$i], $columnSize);
+    }
+
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
 
 foreach($rawBlocks as $block) {
   $key = null;
   preg_match('/\S+(?=:)/', $block, $key);
   $key = trim($key[0]);
-  echo "Yaml block: \n\n" . $block . "\n\n";
-  echo "\nParsed to: \n" . json_encode($parsedYaml[$key], JSON_PRETTY_PRINT) . "\n";
-  echo "-----------------------------------------------------------------\n";
+
+  $rows = blockToFormattedRows($block, $parsedYaml[$key]);
+  
+  foreach($rows as $row) {
+    echo $GREEN . $row['yaml'];
+    echo $BLUE . $row['parsed'];
+    echo "\n";
+  }
+
+  echo $RESET . "\n\n";
 }
 //echo "------\n";
 //echo json_encode($parsedYaml, JSON_PRETTY_PRINT);
